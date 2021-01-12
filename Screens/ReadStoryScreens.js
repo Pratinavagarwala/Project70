d2a2a6b9500dc9405cc3fcbe468c889b0dc46b4c
Pratinav Agarwala <pratinavagarwa2006@gmail.com>
2021-01-12 16:14:31 +0530

    Project70

---
+++ Screens/ReadStoryScreens.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {StyleSheet,View,Text} from 'react-native';
+
+export default class ReadStoryScreen extends React.Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>Read Story</Text>
+            </View>
+        );
+    }
+}
+
+const styles= StyleSheet.create({
+    container:{
+        
+    }
+
+})
